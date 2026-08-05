46e4a4d4b3fc79399e833830b12e7001696ded51
saribethvalderramaplatbrands-boop <saribeth.valderrama.platbrands@gmail.com>
2026-08-05 16:48:55 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL
+const supabaseKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
